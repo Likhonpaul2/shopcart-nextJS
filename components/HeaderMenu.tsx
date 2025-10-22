a95c4a0a8cba25f7f87ec9add7e8f99713dfffae
Likhonpaul2 <likhonpaul2@gmail.com>
2025-10-22 06:46:20 +0600

    Implement search, signIn, CartIcon, Favorite button

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -16,10 +16,10 @@ const HeaderMenu = () => {
         >
           {item?.title}
           <span
-            className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:left-0`}
+            className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:left-0 ${pathName === item?.href && "w-1/2"}`}
           />
           <span
-            className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:right-0 `}
+            className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:right-0 ${pathName === item?.href && "w-1/2"} `}
           />
         </Link>
       ))}
